9bd5cd2a25ffc11b718cce70c581aedc946c47ff
Priyadharshinis0612 <Priyadharshinis.22csd@kongu.edu>
2025-07-08 21:31:19 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import Navbar from './components/navbar/navbar';
+import { Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home/Home.jsx";
+import Cart from "./pages/Cart/Cart.jsx";
+import Placeorder from "./pages/Placeorder/Placeorder.jsx"; 
+import Footer from './components/Footer/Footer.jsx';
+import LoginPopup from './LoginPopup/LoginPopup.jsx';
+const App = () => {
+  const[showLogin,setShowLogin]=useState(false);
+  return (
+    <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
+    <div className='app'>
+      <Navbar setShowLogin={setShowLogin}/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/order' element={<Placeorder />} />
+      </Routes>
+    </div>
+    <Footer/>
+    </>
+    
+  );
+};
+
+export default App;
